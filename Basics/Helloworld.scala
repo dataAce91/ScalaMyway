63d6dcb72866605e26d8a7863c83b0a0c4572a94
dataAce91 <dataace91@gmail.com>
2020-10-27 11:18:25 +0530

    Added basics

---
+++ Basics/Helloworld.scala
@@ -0,0 +1,19 @@
+object Helloworld {  
+// Creating object 
+
+// Scala program to print Hello World 
+  
+
+// Main method  
+def main(args: Array[String])  
+{  
+      
+    // prints Helloworld for u !ello  
+    println("Hello, World!")  
+}  
+}
+
+// to excute the program you need scala installed 
+//Commands to run the program 
+// scalac Helloworld.scala 
+//scala Helloworld
---
+++ README.md
@@ -0,0 +1,6 @@
+# Journey Towards Scala
+
+This repository sums up my journey in learning Scala . Here Im not messing around with with theory to much .Lets dive into  scala 
+Version : 2.12.11 
+# Chapters 
+ 1. Basics - Helloworld ,Variables , Operator
